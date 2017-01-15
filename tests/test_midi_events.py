9ab1306cb27a36705f3df91a916155c44e526107
nocarryr <matt@nomadic-recording.com>
2017-01-15 15:09:16 -0600

    Add serialization tests

--- tests/test_midi_events.py
+++ tests/test_midi_events.py
@@ -1,4 +1,5 @@
 import pytest
+import jsonfactory
 
 from vidhubcontrol.interfaces.midi import events
 
@@ -77,3 +78,44 @@ def test_event_parse(midi_events):
         num_parsed += 1
 
     assert midi_events['num_events'] == num_parsed
+
+def test_serialization(midi_events):
+
+    @jsonfactory.decoder
+    def decode_str_keys(d):
+        keys = [key for key in d if key.isdigit()]
+        if len(keys) == len(d.keys()):
+            return {int(key):val for key, val in d.items()}
+        return d
+
+    # Convert class keys to str
+    d = {'tx_events':{}, 'sysex_events':midi_events['sysex_events']}
+    for ch, _d in midi_events['tx_events'].items():
+        d['tx_events'][ch] = {}
+        for cls, _dd in _d.items():
+            d['tx_events'][ch][str(cls)] = _dd
+
+    s = jsonfactory.dumps(d)
+    deserialized = jsonfactory.loads(s)
+
+    def iter_events(src_obj, test_obj):
+        if isinstance(src_obj, dict):
+            assert isinstance(test_obj, dict)
+            assert set(src_obj.keys()) == set(test_obj.keys())
+            for key in src_obj.keys():
+                yield from iter_events(src_obj[key], test_obj[key])
+        elif isinstance(src_obj, list):
+            assert isinstance(test_obj, list)
+            assert len(src_obj) == len(test_obj)
+            for i in range(len(src_obj)):
+                yield from iter_events(src_obj[i], test_obj[i])
+        else:
+            assert isinstance(src_obj, events.MidiEvent)
+            assert isinstance(test_obj, events.MidiEvent)
+            yield src_obj, test_obj
+
+    num_tested = 0
+    for tx_event, e in iter_events(d, deserialized):
+        assert tx_event == e
+        num_tested += 1
+    assert num_tested == midi_events['num_events']
