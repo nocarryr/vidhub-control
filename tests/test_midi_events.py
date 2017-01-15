5327cdc46faadd814344720512508551e2873976
nocarryr <matt@nomadic-recording.com>
2017-01-14 21:37:37 -0600

    Test Sysex events, add checks for 'is_same_message_type'

--- tests/test_midi_events.py
+++ tests/test_midi_events.py
@@ -3,8 +3,10 @@ from vidhubcontrol.interfaces.midi import events
 def test_event_parse():
     velocity = 90
     tx_events = {}
+    sysex_events = []
     data = []
     num_events = 0
+    sysex_data = list(range(8)) * 8
     for ch in range(16):
         tx_events[ch] = {}
         for value in range(128):
@@ -18,9 +20,48 @@ def test_event_parse():
                     tx_events[ch][cls] = {}
                 tx_events[ch][cls][value] = e
                 num_events += 1
+        sysex_data = [syx + 1 for syx in sysex_data]
+        sysex = events.SysExEvent(data=[0xF0]+sysex_data+[0xF7])
+        data.extend(sysex.build_message())
+        sysex_events.append(sysex)
+        num_events += 1
     num_parsed = 0
+    sysex_iter = iter(sysex_events)
     for e in events.MidiEvent.parse_stream(data):
-        tx_event = tx_events[e.channel][e.__class__][e.value]
-        assert tx_event == e
+        if isinstance(e, events.SysExEvent):
+            tx_event = next(sysex_iter)
+            assert tx_event == e
+        else:
+            tx_event = tx_events[e.channel][e.__class__][e.value]
+            assert tx_event.is_same_message_type(e)
+            assert tx_event == e
+            if isinstance(e, events.NoteEvent):
+                e.velocity += 1
+                assert tx_event.is_same_message_type(e)
+                assert tx_event != e
+                e.value += 1
+                assert not tx_event.is_same_message_type(e)
+                assert tx_event != e
+                e.value = tx_event.value
+                assert tx_event.is_same_message_type(e)
+                e.channel += 1
+                assert not tx_event.is_same_message_type(e)
+                assert tx_event != e
+            else:
+                e.value += 1
+                assert tx_event.is_same_message_type(e)
+                assert tx_event != e
+                e.value = tx_event.value
+                assert tx_event == e
+                e.controller += 1
+                assert not tx_event.is_same_message_type(e)
+                assert tx_event != e
+                e.controller = tx_event.controller
+                assert tx_event.is_same_message_type(e)
+                assert tx_event == e
+                e.channel += 1
+                assert not tx_event.is_same_message_type(e)
+                assert tx_event != e
+
         num_parsed += 1
     assert num_events == num_parsed
