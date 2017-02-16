5f8628be1a6abb82f639c4b33866a5fe367a2988
nocarryr <matt@nomadic-recording.com>
2017-02-16 16:24:35 -0600

    Expand basic kivy test

--- tests/kv/test_kv_app.py
+++ tests/kv/test_kv_app.py
@@ -35,19 +35,35 @@ async def test_vidhub_dropdown(kivy_app):
     assert len(dropdown.btns) == 0
 
     kv_waiter.bind(dropdown, 'btns')
-    vidhub = await DummyBackend.create_async(device_id='dummy1', device_name='Dummy 1')
-    config.add_vidhub(vidhub)
+    vidhub1 = await DummyBackend.create_async(device_id='dummy1', device_name='Dummy 1')
+    vidhub2 = await DummyBackend.create_async(device_id='dummy2', device_name='Dummy 2')
+    config.add_vidhub(vidhub1)
+    await kv_waiter.wait()
+    config.add_vidhub(vidhub2)
     await kv_waiter.wait()
     kv_waiter.unbind(dropdown, 'btns')
 
     assert 'dummy1' in dropdown.btns
+    assert 'dummy2' in dropdown.btns
     btn = dropdown.btns['dummy1']
-    assert btn.text == 'Dummy 1'
+    assert btn.vidhub is vidhub1
+    assert btn.text == vidhub1.device_name == 'Dummy 1'
 
     kv_waiter.bind(kivy_app, 'selected_vidhub')
     btn.dispatch('on_release')
     await kv_waiter.wait()
 
-    assert kivy_app.selected_vidhub is vidhub
+    assert kivy_app.selected_vidhub is vidhub1
+
+    await kivy_app.wait_for_widget_init()
+
+    btn = dropdown.btns['dummy2']
+    assert btn.vidhub is vidhub2
+    assert btn.text == vidhub2.device_name == 'Dummy 2'
+
+    assert not kv_waiter.aio_event.is_set()
+    btn.dispatch('on_release')
+    await kv_waiter.wait()
+    assert kivy_app.selected_vidhub is vidhub2
 
     await kivy_app.stop_async()
