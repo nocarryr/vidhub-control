e39aaa79434e00e12149d393d3632f2b4139cbf9
nocarryr <matt@nomadic-recording.com>
2017-02-21 13:01:17 -0600

    Build and cache all requirement wheels

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -8,22 +8,17 @@ pip install -U Cython==0.23
 # Find cached wheel for kivy
 for file in $KIVY_WHEEL_GLOB ; do
     if [ -e "$file" ] ; then
-        echo "Using cached wheel for kivy: $file"
-        pip install "$file"
+        echo "Found cached wheel for kivy: $file"
         KIVY_BUILD_NEEDED=0
     fi
 done
 
 # Build kivy wheel if not found so it can be cached
 if [ "$KIVY_BUILD_NEEDED" -gt 0 ] ; then
-    echo "Building kivy wheel..."
-    pip wheel --global-option build_ext -r vidhubcontrol/kivyui/requirements.txt -w build/
-    for file in $KIVY_WHEEL_GLOB ; do
-        if [ -e "$file" ] ; then
-            pip install "$file"
-        fi
-    done
+    echo "Building wheels..."
+    pip wheel --build-option build_ext -r requirements-test.txt --wheel-dir=build/
 fi
 
-# Install remaining deps
-pip install -r requirements-test.txt
+# Install from pre-built wheels
+pip install --no-cache-dir --no-index --find-links=build/ kivy
+pip install --no-cache-dir --no-index --find-links=build/ -r requirements-test.txt
