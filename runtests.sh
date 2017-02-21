988df03b8f456c6206646250806d0314fe5659b1
nocarryr <matt@nomadic-recording.com>
2017-02-21 10:54:28 -0600

    Remove --boxed flag from ui test runs

--- runtests.sh
+++ runtests.sh
@@ -7,7 +7,7 @@ mv .coverage .coverage-orig
 for f in tests/kv/test*.py
 do
     echo "Running $f"
-    py.test --cov-append --cov-config .coveragerc --cov=vidhubcontrol --boxed "$f" -s
+    py.test --cov-append --cov-config .coveragerc --cov=vidhubcontrol "$f"
     coverage combine -a .coverage-orig
     mv .coverage .coverage-orig
 done
