5babc78959b56a744bc4d31c146ada9128487b06
nocarryr <matt@nomadic-recording.com>
2017-02-24 15:04:56 -0600

    Add tests for monitor property telnet commands

--- tests/test_telnet_backend.py
+++ tests/test_telnet_backend.py
@@ -4,6 +4,8 @@ import pytest
 from vidhubcontrol.backends import telnet
 from vidhubcontrol.backends.telnet import TelnetBackend, SmartScopeTelnetBackend
 
+from utils import AsyncEventWaiter
+
 @pytest.mark.asyncio
 async def test_telnet_vidhub(mocked_vidhub_telnet_device, vidhub_telnet_responses):
 
@@ -35,6 +37,7 @@ async def test_telnet_smartscope(mocked_vidhub_telnet_device):
     loop.set_debug(True)
 
     backend = await SmartScopeTelnetBackend.create_async(hostaddr=True)
+    waiter = AsyncEventWaiter(backend)
 
     assert backend.prelude_parsed
 
@@ -44,9 +47,55 @@ async def test_telnet_smartscope(mocked_vidhub_telnet_device):
     assert not backend.inverted
     assert backend.num_monitors == len(backend.monitors) == 2
 
-    monitor = backend.monitors[0]
-    assert monitor.name == 'MONITOR A'
-    assert monitor.brightness == 255
-    assert monitor.contrast == 128
+    waiter.bind('on_monitor_property_change')
+
+    async def set_and_check_monitor_prop(obj, prop_name, prop_val):
+        print('{}: setting {} to {}, current={}'.format(obj.name, prop_name, prop_val, getattr(obj, prop_name)))
+        setattr(obj, prop_name, prop_val)
+        args, kwargs = await waiter.wait()
+        _, event_name, event_value = args
+        assert kwargs['monitor'] is obj
+        assert event_name == prop
+        assert event_value == prop_val == getattr(obj, prop)
+
+    defaults = {
+        'brightness':255,
+        'contrast':128,
+        'saturation':128,
+        'identify':False,
+        'border':None,
+        'widescreen_sd':None,
+        'audio_channel':0,
+    }
+    default_scopes = ['waveform', 'vector_100']
+
+    for i, name, monitor in zip(range(2), ['MONITOR A', 'MONITOR B'], backend.monitors):
+        assert monitor.index == i
+        assert monitor.name == name
+
+        for key, val in defaults.items():
+            assert getattr(monitor, key) == val
+        assert monitor.scope_mode == default_scopes[i]
+
+        props = monitor.PropertyChoices._bind_properties
+        for prop in props:
+            choices = monitor.get_property_choices(prop)
+            if choices is not None:
+                for prop_val, device_val in choices.items():
+                    if getattr(monitor, prop) == prop_val:
+                        continue
+                    await set_and_check_monitor_prop(monitor, prop, prop_val)
+            elif prop == 'identify':
+                assert isinstance(monitor.identify, bool)
+                prop_val = not monitor.identify
+                await set_and_check_monitor_prop(monitor, prop, prop_val)
+            else:
+                for i in range(100):
+                    if getattr(monitor, prop) == i:
+                        continue
+                    await set_and_check_monitor_prop(monitor, prop, i)
+                    setattr(monitor, prop, i)
+
+    waiter.unbind()
 
     await backend.disconnect()
