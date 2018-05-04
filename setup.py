d8a307374324c0926ac461161b22585efa80c4f2
nocarryr <matt@nomadic-recording.com>
2018-05-04 14:24:07 -0500

    Move dependencies outside of setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
+import os
 from setuptools import setup, find_packages
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+def get_deps(filename):
+    deps = []
+    with open(os.path.join(BASE_PATH, filename), 'r') as f:
+        for line in f.read().splitlines():
+            if line.startswith('#'):
+                continue
+            deps.append(line)
+    return deps
+
+INSTALL_REQUIRES = get_deps('requirements.txt')
+EXTRAS_REQUIRE = {
+    'kivy':get_deps('vidhubcontrol/kivyui/requirements.txt'),
+}
 
 setup(
     name = "vidhub-control",
@@ -8,22 +25,17 @@ setup(
     description = "Control Smart Videohub Devices",
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
-    install_requires=[
-        'python-dispatch>=0.0.8',
-        'json-object-factory',
-        'zeroconf',
-        'python-osc',
-        'pid',
-    ],
+    install_requires=INSTALL_REQUIRES,
     entry_points={
         'console_scripts':[
             'vidhubcontrol-web = vidhubcontrol.sofi_ui.main:run_app',
             'vidhubcontrol-server = vidhubcontrol.runserver:main',
         ],
         'gui_scripts':[
-            'vidhubcontrol-ui = vidhubcontrol.kivyui.main:main',
+            'vidhubcontrol-ui = vidhubcontrol.kivyui.main:main [kivy]',
         ],
     },
+    extras_require=EXTRAS_REQUIRE,
     platforms=['any'],
     classifiers = [
         'Development Status :: 3 - Alpha',
