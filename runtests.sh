353401d599a7cdfca09498cff3740fee6e68efba
nocarryr <matt@nomadic-recording.com>
2021-05-24 11:06:30 -0500

    Re-enable kivy tests in ci

--- runtests.sh
+++ runtests.sh
@@ -2,14 +2,14 @@
 set -e
 
 py.test --cov-config .coveragerc --cov=vidhubcontrol
-# mv .coverage .coverage-orig
-#
-# for f in tests/kv/test*.py
-# do
-#     echo "Running $f"
-#     py.test --fulltrace --cov-append --cov-config .coveragerc --cov=vidhubcontrol "$f"
-#     coverage combine -a .coverage-orig
-#     mv .coverage .coverage-orig
-# done
-# coverage combine -a .coverage-orig
-# coverage report
+mv .coverage .coverage-orig
+
+for f in tests/kv/test*.py
+do
+    echo "Running $f"
+    py.test --fulltrace --cov-append --cov-config .coveragerc --cov=vidhubcontrol "$f"
+    coverage combine -a .coverage-orig
+    mv .coverage .coverage-orig
+done
+coverage combine -a .coverage-orig
+coverage report
