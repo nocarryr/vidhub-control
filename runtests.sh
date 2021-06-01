047b5b0e5ac3cc1edd7446b2aea8f8c0df407670
nocarryr <matt@nomadic-recording.com>
2021-06-01 15:44:23 -0500

    Add workflow to test from wheel and sdist

--- runtests.sh
+++ runtests.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 set -e
 
-py.test --cov-config .coveragerc --cov=vidhubcontrol
-mv .coverage .coverage-orig
+if [ "$1" = "--no-cov" ]; then
+    COVARG=""
+else
+    COVARG="--cov"
+fi
+
+py.test --cov-config .coveragerc $COVARG
+if [ "$COVARG" != "" ]; then
+    mv .coverage .coverage-orig
+fi
 
 for f in tests/kv/test*.py
 do
     echo "Running $f"
-    py.test --fulltrace --cov-append --cov-config .coveragerc --cov=vidhubcontrol "$f"
-    coverage combine -a .coverage-orig
-    mv .coverage .coverage-orig
+    py.test --fulltrace --cov-append --cov-config .coveragerc "$f" $COVARG
+    if [ "$COVARG" != "" ]; then
+        coverage combine -a .coverage-orig
+        mv .coverage .coverage-orig
+    fi
 done
-coverage combine -a .coverage-orig
-coverage report
+
+if [ "$COVARG" != "" ]; then
+    coverage combine -a .coverage-orig
+    coverage report
+fi
